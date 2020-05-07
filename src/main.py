862d8c542f86ccfc3c641008747e43e5854e06f2
stefanpatrichi <stefanpatrichi07@gmail.com>
2020-05-07 13:27:33 +0300

    remove main function

--- src/main.py
+++ src/main.py
@@ -1,17 +1,10 @@
 # imports
-
-import schedule
 import time
 
 from get_json import get_json
 from cases import write_cases
 from plot import plot
 
-# main function
-
-def main():
-    get_json()
-    write_cases()
-    plot()
-
-main()
+get_json()
+write_cases()
+plot()
